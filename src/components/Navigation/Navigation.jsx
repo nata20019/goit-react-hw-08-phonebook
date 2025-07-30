290b7b8da2eae84a675c7c7e6ba075327cff124a
nata20019 <150694271+nata20019@users.noreply.github.com>
2025-07-30 18:07:04 +0200

    mod842312365

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,8 +1,10 @@
 import { NavLink } from 'react-router-dom';
 import css from './Navigation.module.css';
+import { useSelector } from 'react-redux'; // Import useSelector
+import { selectIsLoggedIn } from '../../redux/auth/selectors'; // Correct path to selectors
 
 export const Navigation = () => {
-  const isLoggedIn = false;
+  const isLoggedIn = useSelector(selectIsLoggedIn); // Get isLoggedIn from Redux
 
   return (
     <nav>
--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,15 +1,21 @@
 import { useDispatch, useSelector } from 'react-redux';
 import css from './UserMenu.module.css';
 import { logOut } from '../../redux/auth/operations';
+import { clearContacts } from '../../redux/contactsSlice'; // Import clearContacts
 
 export const UserMenu = () => {
   const dispatch = useDispatch();
   const user = useSelector(state => state.auth.user);
 
+  const handleLogout = () => {
+    dispatch(logOut());
+    dispatch(clearContacts()); // Clear contacts on logout
+  };
+
   return (
     <div className={css.wrapper}>
       <p className={css.username}>Welcome, {user.name}</p>
-      <button type="button" onClick={() => dispatch(logOut())}>
+      <button type="button" onClick={handleLogout}>
         Logout
       </button>
     </div>
